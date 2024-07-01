6b344d96ce80b933c8fb91eba08ecf369fa7e363
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-01 11:49:05 +0000

    Add Profile model

--- skill_sharing_platform/masteryhub/models.py
+++ skill_sharing_platform/masteryhub/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    skills = models.CharField(max_length=255, blank=True)
+    experience = models.TextField(blank=True)
+    achievements = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
