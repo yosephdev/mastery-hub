ade57a9910aaaaf4263b10bf32aaf51868c7d62f
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-01 11:52:59 +0000

    Add Payment model

--- skill_sharing_platform/masteryhub/models.py
+++ skill_sharing_platform/masteryhub/models.py
@@ -44,3 +44,15 @@ class Mentorship(models.Model):
 
     def __str__(self):
         return f"{self.mentor.username} mentoring {self.mentee.username}"
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    session = models.ForeignKey(
+        Session, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} on {self.date}"
