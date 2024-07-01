7198608b74e9e0ec92c44f53d5d23182221d9591
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-01 11:50:16 +0000

    Add Session model

--- skill_sharing_platform/masteryhub/models.py
+++ skill_sharing_platform/masteryhub/models.py
@@ -13,3 +13,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Session(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    duration = models.DurationField()
+    host = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sessions_hosted"
+    )
+    participants = models.ManyToManyField(
+        User, related_name="sessions_participated", blank=True
+    )
+
+    def __str__(self):
+        return self.title
