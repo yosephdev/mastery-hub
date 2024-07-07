86672d165bccc1853c1f34a121f07eeff1c6dceb
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-07 14:42:13 +0000

    Add URL patterns for user authentication views

--- masteryhub/urls.py
+++ masteryhub/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
-    path('', views.home, name='masteryhub'), 
+    path("", views.home, name="masteryhub"),
+    path("signup/", views.signup_view, name="account_signup"),
+    path("login/", views.CustomLoginView.as_view(), name="account_login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="account_logout"),
 ]
