23a24174cb2419d46e4b3420ac5c73a073b5304a
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-17 11:08:44 +0000

    Set Debug to True

--- skill_sharing_platform/settings.py
+++ skill_sharing_platform/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
