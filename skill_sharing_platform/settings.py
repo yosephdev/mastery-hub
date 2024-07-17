afe8bb75f857b0825abab1d33cfb32d0b3edfc9e
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-17 13:13:34 +0000

    Import python-dotenv to settings.py

--- skill_sharing_platform/settings.py
+++ skill_sharing_platform/settings.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import dj_database_url
 from django.contrib.messages import constants as messages
 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
