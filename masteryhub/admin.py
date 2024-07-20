3ec42245abbaa6c958d4e30c1e56b3fe74a28fbe
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-20 07:02:41 +0000

    Log mentor requests in admin using LogEntry

--- masteryhub/admin.py
+++ masteryhub/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Session, Profile, Category, Mentorship, Review, Payment, Forum
+from django.contrib.admin.models import LogEntry
 
 # Register your models here.
 
+
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ("user", "action_time", "content_type", "object_repr", "action_flag")
+    list_filter = ("action_time", "content_type", "user")
+    search_fields = ("object_repr", "change_message")
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
 admin.site.register(Session)
 admin.site.register(Profile)
 admin.site.register(Category)
