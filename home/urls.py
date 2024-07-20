d9a1da4f1d53135a3fdf882083fe2201297d9ca6
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-20 16:08:05 +0000

    Add URL patterns for about and contact pages in home/urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import contact_view
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home'),        
+    path("about/", views.about, name="about"),  
+    path('contact/', contact_view, name='contact'),
 ]
