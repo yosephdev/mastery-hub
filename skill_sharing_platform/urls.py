d82462ab21296f5ada3c71482dde03b987a3ae5c
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-07-29 16:42:20 +0000

    Add a custom 404 page

--- skill_sharing_platform/urls.py
+++ skill_sharing_platform/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404
 
 
 urlpatterns = [
@@ -35,3 +36,5 @@ if settings.DEBUG:
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'skill_sharing_platform.views.handler404'
