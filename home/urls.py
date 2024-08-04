fc062b7f433c6ce4513379c569a8caf0c080bb3b
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-08-04 12:53:42 +0000

    Add URL pattern for search view in home app urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .views import contact_view
-from .views import (  
-    home,  
-)
+from .views import home, contact_view, search
 
 urlpatterns = [
     path("", views.index, name="home"),
     path("", home, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", contact_view, name="contact"),
+    path("search/", search, name="search"),
 ]
