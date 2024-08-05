bf0cda151f6719d85abc33fbf2cbee940864917e
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-08-05 10:24:28 +0000

    Add custom 404 error page

--- skill_sharing_platform/urls.py
+++ skill_sharing_platform/urls.py
@@ -21,7 +21,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import handler404
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
@@ -31,9 +30,6 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path("", include("home.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
