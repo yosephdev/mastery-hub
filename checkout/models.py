4114a279cab284c3aaceada0bed43b6a9b083632
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-08-05 07:35:56 +0000

    Update Order model to use CountryField for country selection handling

--- checkout/models.py
+++ checkout/models.py
@@ -1,9 +1,14 @@
+import uuid
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
 from masteryhub.models import Session
 from accounts.models import Profile
 
 # Create your models here.
+
 
 class Payment(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -16,30 +21,34 @@ class Payment(models.Model):
     def __str__(self):
         return f"{self.user.user.username} - {self.amount} on {self.date}"
 
+
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_number = models.CharField(max_length=32, unique=True)
     date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True, null=True)
     street_address1 = models.CharField(max_length=80)
     street_address2 = models.CharField(max_length=80, blank=True)
     county = models.CharField(max_length=80, blank=True)
     town_or_city = models.CharField(max_length=80)
     postcode = models.CharField(max_length=20, blank=True)
-    country = models.CharField(max_length=40)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     phone_number = models.CharField(max_length=20)
     order_total = models.DecimalField(max_digits=10, decimal_places=2)
     delivery_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
         return self.order_number
