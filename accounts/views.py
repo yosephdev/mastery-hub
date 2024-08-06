28bd776799f2835f34600d1cdc86a8fc4fbd9771
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-08-06 15:09:16 +0000

    Fix signup form handling by adding request parameter to save method

--- accounts/views.py
+++ accounts/views.py
@@ -54,7 +54,7 @@ def signup_view(request):
     if request.method == "POST":
         form = CustomSignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(request)
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=password)
