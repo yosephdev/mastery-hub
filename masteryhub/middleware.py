89463a5dee9136c533f35456c10f0a7557b038f0
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-08-08 14:04:09 +0000

    Fix SyntaxError in BrotliMiddleware

--- masteryhub/middleware.py
+++ masteryhub/middleware.py
@@ -5,10 +5,9 @@ from django.http import HttpResponse
 
 class BrotliMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        if 'br' in request.headers.get
-        ('Accept-Encoding', '') and isinstance(response, HttpResponse):
-            if not response.streaming and 'Content-Encoding' not in response:
+        accept_encoding = request.headers.get("Accept-Encoding", "")
+        if "br" in accept_encoding and isinstance(response, HttpResponse):
+            if not response.streaming and "Content-Encoding" not in response:
                 response.content = brotli.compress(response.content)
-                response['Content-Encoding'] = 'br'
+                response["Content-Encoding"] = "br"
         return response
-
