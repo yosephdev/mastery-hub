81556f99cae80250106a716ea9fb492e3cdc8a11
yosephdev <yosephbet@gmail.com>
2024-11-20 16:47:40 +0100

    Adjust the cart_total function in context_processors.py

--- checkout/context_processors.py
+++ checkout/context_processors.py
@@ -2,10 +2,10 @@ from .models import Cart
 
 
 def cart_total(request):
+    """Context processor to get the total number of items in the user's cart."""
+    total_items = 0
     if request.user.is_authenticated:
-        cart, created = Cart.objects.get_or_create(user=request.user)
-        total = sum(
-            item.session.price * item.quantity for item in cart.cartitem_set.all()
-        )
-        return {"grand_total": total}
-    return {"grand_total": 0}
+        cart = Cart.objects.filter(user=request.user).first()  
+        if cart:
+            total_items = cart.items.count() 
+    return {'cart_total': total_items}
