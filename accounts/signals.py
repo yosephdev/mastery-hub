ad074bee86a0733bc08c489219fa63061f4007c9
yosephdev <yosephbet@gmail.com>
2024-11-20 13:52:06 +0100

    Add signal handlers in signals.py to automatically create and save user profiles.

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,10 +9,12 @@ logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """Create a user profile when a new user is created."""
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    """Save the user profile when the user is saved."""
     instance.profile.save()
