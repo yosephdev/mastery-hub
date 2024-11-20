d2e0f42e9836a386098b2abd15e11f6c6bf591db
yosephdev <yosephbet@gmail.com>
2024-11-20 13:52:06 +0100

    Refactor models.py to improve structure and relationships for checkout functionality.

--- checkout/models.py
+++ checkout/models.py
@@ -2,7 +2,6 @@ import uuid
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 from django.db import models
-from django.db.models import Sum
 from django.conf import settings
 from masteryhub.models import Session
 from profiles.models import Profile
@@ -14,9 +13,7 @@ class Payment(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
-    session = models.ForeignKey(
-        Session, on_delete=models.SET_NULL, null=True, blank=True
-    )
+    session = models.ForeignKey(Session, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.user.username} - {self.amount} on {self.date}"
@@ -53,8 +50,7 @@ class Order(models.Model):
     country = CountryField(blank_label="Country *", null=False, blank=False)
     phone_number = models.CharField(max_length=20)
     order_total = models.DecimalField(max_digits=10, decimal_places=2)
-    delivery_cost = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0)
+    delivery_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
