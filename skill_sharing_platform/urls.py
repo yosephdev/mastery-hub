82fe3f72dec51627da5c91b21bf18d45cbbccd99
yosephdev <yosephbet@gmail.com>
2024-11-20 13:52:07 +0100

    Refactor urls.py to streamline routing for the application.

--- skill_sharing_platform/urls.py
+++ skill_sharing_platform/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "skill_sharing_platform.views.handler404"
