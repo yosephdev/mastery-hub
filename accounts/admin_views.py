c2e01352c834cab8411a3e0f3b5ef873ce8bdb05
yosephdev <yosephbet@gmail.com>
2024-11-20 13:52:06 +0100

    Enhance admin_views.py with additional functionality for user management.

--- accounts/admin_views.py
+++ accounts/admin_views.py
@@ -11,9 +11,7 @@ def admin_dashboard(request):
     context = {
         "title": "MasteryHub Admin Dashboard",
         "subtitle": "Welcome to the Admin Dashboard",
-        "latest_users": User.objects.filter(is_staff=False).order_by("-date_joined")[
-            :5
-        ],
+        "latest_users": User.objects.filter(is_staff=False).order_by("-date_joined")[:5],
         "total_users": User.objects.count(),
     }
     return render(request, "admin/dashboard.html", context)
