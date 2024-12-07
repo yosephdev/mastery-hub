c825cd77326ea7a737e95cac0acb517f1220c3e3
yosephdev <yosephbet@gmail.com>
2024-12-07 19:15:23 +0100

    Update email templates and settings
    - Remove redundant password_reset_email.html template
    - Update context processors for checkout
    - Adjust Django settings configuration

--- skill_sharing_platform/settings.py
+++ skill_sharing_platform/settings.py
@@ -2,7 +2,6 @@ import os
 import sys
 from pathlib import Path
 import dj_database_url
-from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -183,18 +182,21 @@ if DEBUG:
     EMAIL_HOST = "localhost"
     EMAIL_PORT = 1025
 else:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_PORT = 587
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+DEFAULT_FROM_EMAIL = 'masteryhub@example.com'
 
 # Stripe Configuration
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
 # Additional Settings
 SITE_ID = 1
