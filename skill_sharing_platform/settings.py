291b4a73f70a774f45b6d3d0b570cf8558c2aaf4
yosephdev <yosephbet@gmail.com>
2025-01-09 18:37:21 +0100

    chore: Update development settings for local testing

--- skill_sharing_platform/settings.py
+++ skill_sharing_platform/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Core Django Settings
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = False
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 ROOT_URLCONF = "skill_sharing_platform.urls"
 WSGI_APPLICATION = "skill_sharing_platform.wsgi.application"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
