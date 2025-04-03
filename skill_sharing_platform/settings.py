b8228ad7311a6e00bc8f1a1bbd4066056f2309d3
yosephdev <yosephbet@gmail.com>
2025-04-03 07:56:00 +0200

    fix: switch email backend from Gmail to Postmark for production email sending

--- skill_sharing_platform/settings.py
+++ skill_sharing_platform/settings.py
@@ -166,15 +166,12 @@ if DEBUG or 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'masteryhub@example.com'
     SERVER_EMAIL = DEFAULT_FROM_EMAIL
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'yosephbet@gmail.com')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = 'masteryhub@example.com'
-    SERVER_EMAIL = DEFAULT_FROM_EMAIL
+    EMAIL_BACKEND = 'postmark.django.backend.EmailBackend'
+    POSTMARK_API_KEY = os.environ.get('POSTMARK_API_KEY')
 
+    DEFAULT_FROM_EMAIL = 'contact@yoseph.dev'
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL
+  
 # Email Subject Prefix
 EMAIL_SUBJECT_PREFIX = '[MasteryHub] '
 
